f43ba8339221f2afd987a15e13012f7e28623194
saoxia <407469895@qq.com>
2023-10-07 15:21:15 +0800

    初始化

---
+++ erpnext_china_mdm/setup/after_install/operations/install_fixtures.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+# License: GNU General Public License v3. See license.txt
+
+
+import os
+from pathlib import Path
+
+import frappe
+from frappe import _
+from frappe.desk.page.setup_wizard.setup_wizard import make_records
+
+
+def install(country='China'):
+	pass
