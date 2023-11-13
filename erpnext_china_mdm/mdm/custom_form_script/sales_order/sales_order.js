19d941dd8c607487881ebd4df61d784018e2fbad
digitwise <407469895@qq.com>
2023-11-13 11:21:27 +0800

    Update sales_order.js

--- erpnext_china_mdm/mdm/custom_form_script/sales_order/sales_order.js
+++ erpnext_china_mdm/mdm/custom_form_script/sales_order/sales_order.js
@@ -4,7 +4,7 @@ frappe.ui.form.on('Sales Order', {
 		frm.remove_custom_button(__('Purchase Order'), __('Create'));
 		frm.remove_custom_button(__('Work Order'), __('Create'));
 		frm.remove_custom_button(__('Material Request'), __('Create'));
-		frm.remove_custom_button(__('Request for Raw Materials') __('Create'));
+		frm.remove_custom_button(__('Request for Raw Materials'), __('Create'));
 		frm.remove_custom_button(__('Purchase Order'), __('Create'));
 		frm.remove_custom_button(__('Payment Request'), __('Create'));
 		}
