855cac7b88dfe1602ccedfbd104c6f200d2f5410
digitwise <407469895@qq.com>
2023-11-14 16:03:04 +0800

    Update delivery_note.js

--- erpnext_china_mdm/mdm/custom_form_script/delivery_note/delivery_note.js
+++ erpnext_china_mdm/mdm/custom_form_script/delivery_note/delivery_note.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 // License: GNU General Public License v3. See license.txt
 
-frappe.ui.form.on('Sales Order', {
+frappe.ui.form.on('Delivery Note', {
 	onload(frm) {
 		// 删除按钮
 		frm.remove_custom_button(__('Delivery Trip'), __('Create'));
