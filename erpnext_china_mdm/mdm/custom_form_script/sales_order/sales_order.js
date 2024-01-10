cde561a1f6061210e23fc0bf9d206d628276a782
digitwise <407469895@qq.com>
2024-01-10 15:15:41 +0800

    销售订单选择物料时，只选择已经定义的UOM

--- erpnext_china_mdm/mdm/custom_form_script/sales_order/sales_order.js
+++ erpnext_china_mdm/mdm/custom_form_script/sales_order/sales_order.js
@@ -9,5 +9,32 @@ frappe.ui.form.on('Sales Order', {
 		frm.remove_custom_button(__('Payment Request'), __('Create'));
 		frm.remove_custom_button(__('Maintenance Visit'), __('Create'));
 		frm.remove_custom_button(__('Maintenance Schedule'), __('Create'));
+		};
+
+	refresh(frm) {
+			// 销售订单选择物料时，只选择已经定义的UOM
+			frm.fields_dict['items'].grid.get_field('uom').get_query = function(doc, cdt, cdn){
+				var row = locals[cdt][cdn];
+				return {
+					query:"erpnext.accounts.doctype.pricing_rule.pricing_rule.get_item_uoms",
+					filters: {'value':row.item_code, apply_on:"Item Code"},
+					
+				}
 		}
+		}
+
 	})
+
+
+
+		refresh(frm) {
+			// your code here
+			frm.fields_dict['items'].grid.get_field('uom').get_query = function(doc, cdt, cdn){
+				var row = locals[cdt][cdn];
+				return {
+					query:"erpnext.accounts.doctype.pricing_rule.pricing_rule.get_item_uoms",
+					filters: {'value':row.item_code, apply_on:"Item Code"},
+					
+				}
+		}
+		}
