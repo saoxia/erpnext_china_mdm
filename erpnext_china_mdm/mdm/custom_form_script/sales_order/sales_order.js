067220fd63a860d25e9bdf9abc3dfec991ec0894
digitwise <407469895@qq.com>
2024-01-10 17:23:44 +0800

    Update sales_order.js

--- erpnext_china_mdm/mdm/custom_form_script/sales_order/sales_order.js
+++ erpnext_china_mdm/mdm/custom_form_script/sales_order/sales_order.js
@@ -17,8 +17,6 @@ frappe.ui.form.on('Sales Order', {
 			return {
 				query:"erpnext.accounts.doctype.pricing_rule.pricing_rule.get_item_uoms",
 				filters: {'value':row.item_code, apply_on:"Item Code"},
-				
 			}
 		};
-
-	})
+	}})
