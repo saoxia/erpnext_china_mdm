0d1dc69f182e040d976b4f07e5cfb5e4ac61d0d1
digitwise <407469895@qq.com>
2024-01-10 16:28:26 +0800

    Update sales_order.js

--- erpnext_china_mdm/mdm/custom_form_script/sales_order/sales_order.js
+++ erpnext_china_mdm/mdm/custom_form_script/sales_order/sales_order.js
@@ -11,16 +11,14 @@ frappe.ui.form.on('Sales Order', {
 		frm.remove_custom_button(__('Maintenance Schedule'), __('Create'));
 		};
 
-	refresh(frm) {
-			// 销售订单选择物料时，只选择已经定义的UOM
-			frm.fields_dict['items'].grid.get_field('uom').get_query = function(doc, cdt, cdn){
-				var row = locals[cdt][cdn];
-				return {
-					query:"erpnext.accounts.doctype.pricing_rule.pricing_rule.get_item_uoms",
-					filters: {'value':row.item_code, apply_on:"Item Code"},
-					
-				}
-		}
+		// 销售订单选择物料时，只选择已经定义的UOM
+		frm.fields_dict['items'].grid.get_field('uom').get_query = function(doc, cdt, cdn){
+			var row = locals[cdt][cdn];
+			return {
+				query:"erpnext.accounts.doctype.pricing_rule.pricing_rule.get_item_uoms",
+				filters: {'value':row.item_code, apply_on:"Item Code"},
+				
+			}
 		}
 
 	})
