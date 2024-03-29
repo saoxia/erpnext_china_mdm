5573b65ec77c79b0bc9aa947e88dacf93f2c3821
digitwise <407469895@qq.com>
2024-03-29 16:20:09 +0800

    Update permission_item_group.py

--- erpnext_china_mdm/mdm/custom_permission/item_group/permission_item_group.py
+++ erpnext_china_mdm/mdm/custom_permission/item_group/permission_item_group.py
@@ -40,7 +40,7 @@ def has_query_permission(user):
 	return conditions
 
 def has_permission(doc, user, permission_type=None):
-	if frappe.db.get_value('Has Role',{'parent':user,'role':['in',['System Manager','销售会计','销售支持']]}):
+	if frappe.db.get_value('Has Role',{'parent':user,'role':['in',['System Manager','销售会计','销售支持','仓库','仓库管理']]}):
 		# 如果角色包含管理员，则看到全量
 		return True
 	elif frappe.db.get_value('Has Role',{'parent':user,'role':['in',['销售']]}):
