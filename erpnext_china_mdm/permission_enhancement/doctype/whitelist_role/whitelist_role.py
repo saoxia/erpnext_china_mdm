20d1e8ba1c9fc9ea76b2235dc115c79649425769
digitwise <407469895@qq.com>
2024-04-07 11:49:57 +0800

    Revert "权限"
    This reverts commit 7cdee821bf79b316e3b215776542eefe0313c327.

--- erpnext_china_mdm/permission_enhancement/doctype/whitelist_role/whitelist_role.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2024, Fisher and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class WhitelistRole(Document):
-	pass
