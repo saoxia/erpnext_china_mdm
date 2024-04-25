5d064e3977e541dcf8132eb1b7237bff26b1690b
digitwise <407469895@qq.com>
2024-04-25 13:02:33 +0800

    Create stock_entry.py

---
+++ erpnext_china_mdm/mdm/custom_form_script/stock_entry.py
@@ -0,0 +1,8 @@
+from erpnext.stock.doctype.stock_entry import StockEntry
+
+class CustomStockEntry(StockEntry):
+	@property
+	def custom_item_uoms_string(self):
+		item_code = self.items.item_code
+		item_doc = frappe.get_doc('Item', item_code)
+		return item_doc.custom_uoms_string
