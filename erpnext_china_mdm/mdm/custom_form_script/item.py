9bc069dbb7c6a9721a599a9ff08daa5e5de15ac8
digitwise <407469895@qq.com>
2024-05-04 08:57:22 +0800

    Update item.py

--- erpnext_china_mdm/mdm/custom_form_script/item.py
+++ erpnext_china_mdm/mdm/custom_form_script/item.py
@@ -3,20 +3,26 @@ from erpnext.stock.doctype.item.item import Item
 class CustomItem(Item):
     def set_custom_uoms_string(self):
         uoms = self.uoms
-        # 从小到大排序
         uom_dict = {}
         for idx in uoms:
             uom = idx.as_dict()['uom']
             conversion_factor = idx.as_dict()['conversion_factor']
             uom_dict.update({conversion_factor:uom})
-        #uom_dict = {key: uom_dict[key] for key in sorted(uom_dict.keys())}
+        uom_dict = {key: uom_dict[key] for key in sorted(uom_dict.keys())}
         s = ''
         last_uom = None
         last_conversion_factor = None
         for conversion_factor,uom in uom_dict.items():
             if (conversion_factor - int(conversion_factor)) == 0 :
                 conversion_factor = int(conversion_factor)
-            if self.stock_uom != uom and conversion_factor >= 1:
+            if self.stock_uom != uom and conversion_factor <= 1
+                if last_uom:
+                    if (conversion_factor/last_conversion_factor - int(conversion_factor/last_conversion_factor)) == 0 :
+                        conversion_factor_last_conversion_factor = int(conversion_factor/last_conversion_factor)
+                    s = f'{s};{conversion_factor_last_conversion_factor}{last_uom}/{uom}'
+                else:
+                    s = f'{s};{conversion_factor}{self.stock_uom}/{uom}'
+            elif self.stock_uom != uom and conversion_factor >= 1:
                 if last_uom:
                     if (conversion_factor/last_conversion_factor - int(conversion_factor/last_conversion_factor)) == 0 :
                         conversion_factor_last_conversion_factor = int(conversion_factor/last_conversion_factor)
@@ -25,6 +31,8 @@ class CustomItem(Item):
                     s = f'{s};{conversion_factor}{self.stock_uom}/{uom}'
             last_uom = uom
             last_conversion_factor = conversion_factor
+            else:
+                pass
         self.custom_uoms_string = s[1:]
 
     def before_save(self):
