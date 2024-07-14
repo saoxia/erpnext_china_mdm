aa811833008031692eee25307c7a8bbf9b0ce191
saoxia <407469895@qq.com>
2024-07-14 09:55:24 +0800

    Update permission_lead.py

--- erpnext_china_mdm/mdm/custom_permission/lead/permission_lead.py
+++ erpnext_china_mdm/mdm/custom_permission/lead/permission_lead.py
@@ -11,10 +11,10 @@ def has_query_permission(user):
 		users = get_employee_tree(parent=user)
 		users.append(user)
 		users_str = str(tuple(users)).replace(',)',')')
-		conditions = f"(owner in {users_str}) or (lead_owner in {users_str})" 
+		conditions = f"(owner in {users_str}) or (lead_owner in {users_str}) or (custom_sea = '公海')" 
 		if len(frappe.get_all('Has Role', {'parent':user,'role': ['in', ['网络推广', '销售']]})) > 0:
 			# conditions = f"(custom_sea='公海') or (owner in {users_str}) or (lead_owner in {users_str})" 
-			conditions = f"(owner in {users_str}) or (lead_owner in {users_str})" 
+			conditions = f"(owner in {users_str}) or (lead_owner in {users_str}) or (custom_sea = '公海')" 
 	return conditions
 
 def has_permission(doc, user, permission_type=None):
@@ -26,7 +26,7 @@ def has_permission(doc, user, permission_type=None):
 		users = get_employee_tree(parent=user)
 		users.append(user)
 		# if (doc.owner in users) or (doc.lead_owner in users) or (doc.custom_sea == "公海" and len(frappe.get_all('Has Role', {'parent':user,'role': ['in', ['网络推广', '销售']]})) > 0):
-		if (doc.owner in users) or (doc.lead_owner in users) or (len(frappe.get_all('Has Role', {'parent':user,'role': ['in', ['网络推广', '销售']]})) > 0):
+		if (doc.owner in users) or (doc.lead_owner in users) or (len(frappe.get_all('Has Role', {'parent':user,'role': ['in', ['网络推广', '销售']]})) > 0) or (doc.custom_sea == '公海'):
 			return True
 		else:
 			return False
